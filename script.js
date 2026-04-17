ab3d1b34f65761dc0899aa82d09684f22e67f216
Whovencroft <whovencroft@gmail.com>
2026-04-17 19:09:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.site-nav');
+const page = document.body.dataset.page;
 
 if (menuToggle && nav) {
   menuToggle.addEventListener('click', () => {
@@ -8,6 +10,10 @@ if (menuToggle && nav) {
   });
 
   nav.querySelectorAll('a').forEach((link) => {
+    if (page && link.dataset.page === page) {
+      link.classList.add('is-current');
+    }
+
     link.addEventListener('click', () => {
       nav.classList.remove('is-open');
       menuToggle.setAttribute('aria-expanded', 'false');
